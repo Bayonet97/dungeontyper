078a8d59d84f2fd99ce385682ad56b6a68c9763e
Akkerveken,Raphaël R. van <r.vanakkerveken@student.fontys.nl>
2018-12-06 14:38:11 +0100

    Stored Procedures added

---
+++ DungeonTyper/DungeonTyper.Web/Data/DataBase/StoredProcedures/GetCharacterClass.sql
@@ -0,0 +1,31 @@
+﻿-- =============================================
+-- Author:        <Stef Boelens>
+-- Create date: <dd/mm/yyyy>
+-- Description:    <Buitendoor, Killerapp Semester 2, >
+-- =============================================
+
+USE [dbi397017]
+GO
+
+IF EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_NAME = 'stp_ProcedureName' AND ROUTINE_SCHEMA = 'S2Buitendoor') DROP PROCEDURE [S2Buitendoor].stp_ProcedureName
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [S2Buitendoor].[stp_ProcedureName]
+    -- Add the parameters for the stored procedure here
+    @Param1 varchar(50),
+    @Param2 int
+AS
+BEGIN
+    -- SET NOCOUNT ON added to prevent extra result sets from
+    -- interfering with SELECT statements.
+    SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+    
+END
+GO
---
+++ DungeonTyper/DungeonTyper.Web/Data/DataBase/Tables/BuildCharacterClassTable.sql
@@ -0,0 +1,14 @@
+﻿IF EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'GebruikersKampen' AND TABLE_SCHEMA = 'S2Buitendoor') DROP TABLE S2Buitendoor.GebruikersKampen
+GO
+
+CREATE TABLE [S2Buitendoor].[Gebruikers] (
+    [Id]              INT           IDENTITY (1, 1) NOT NULL,
+    [Naam]            VARCHAR (50)  NOT NULL,
+    [Wachtwoord]      VARCHAR (100) NOT NULL,
+    [Email]           VARCHAR (50)  NOT NULL,
+    [Administrator]   BIT           CONSTRAINT [DF_Gebruikers_Administrator] DEFAULT ((0)) NOT NULL,
+    [UpdateTimeValue] INT           CONSTRAINT [DF_Gebruikers_UpdateTimeValue] DEFAULT ((5)) NOT NULL,
+    [SeeAllItems]     BIT           CONSTRAINT [DF_Gebruikers_SeeAllItems] DEFAULT ((0)) NOT NULL,
+    [FirstLogin]      BIT           CONSTRAINT [DF_Gebruikers_FirstLogin] DEFAULT ((1)) NOT NULL,
+    CONSTRAINT [PK_Gebruikers] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
