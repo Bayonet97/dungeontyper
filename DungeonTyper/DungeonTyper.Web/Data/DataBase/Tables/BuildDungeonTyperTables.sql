89392d98bfdc993682ce3698f9696e160c64cf19
Raphael <43172440+Raphael@users.noreply.github.com>
2018-12-09 00:29:30 +0100

    Finsihed table SQL for Character and CharacterClass

---
+++ DungeonTyper/DungeonTyper.Web/Data/DataBase/Tables/BuildDungeonTyperTables.sql
@@ -0,0 +1,24 @@
+﻿USE [dbi397017]
+GO
+
+IF EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Character' AND TABLE_SCHEMA = 'DungeonTyper') DROP TABLE [DungeonTyper].[Character]
+GO
+IF EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'CharacterClass' AND TABLE_SCHEMA = 'DungeonTyper') DROP TABLE [DungeonTyper].[CharacterClass]
+GO
+
+
+CREATE TABLE [DungeonTyper].[CharacterClass] (
+    [Id]              INT           IDENTITY (1, 1) NOT NULL,
+    [ClassName]       VARCHAR (20)  NOT NULL,
+    CONSTRAINT [PK_CharacterClass] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [DungeonTyper].[Character] (
+    [Id]                INT    IDENTITY (1, 1) NOT NULL,
+    [CharacterClassId]  INT    NOT NULL,
+	[Alive]				BIT    NOT NULL,
+    CONSTRAINT [PK_Character] PRIMARY KEY CLUSTERED ([Id] ASC),
+	CONSTRAINT [FK_Character_CharacterClass] FOREIGN KEY ([CharacterClassId]) REFERENCES [DungeonTyper].[CharacterClass] ([Id])
+);
+
+GO
---
+++ DungeonTyper/DungeonTyper.Web/Data/DataBase/Tables/FillDungeonTyperTables.sql
@@ -0,0 +1,18 @@
+﻿USE [dbi397017]
+GO
+
+--DELETE DATA
+DELETE FROM [DungeonTyper].[Character]
+DELETE FROM [DungeonTyper].[CharacterClass]
+
+--INSERT
+
+--	CHARACTERCLASS
+INSERT INTO [DungeonTyper].[CharacterClass]([ClassName]) VALUES('Warrior')
+INSERT INTO [DungeonTyper].[CharacterClass]([ClassName]) VALUES('Mage')
+INSERT INTO [DungeonTyper].[CharacterClass]([ClassName]) VALUES('Rogue')
+INSERT INTO [DungeonTyper].[CharacterClass]([ClassName]) VALUES('Priest')
+
+--	CHARACTER (foreign key CharacterClassId)
+INSERT INTO [DungeonTyper].[Character]([CharacterClassId], [Alive]) VALUES (1, 1)
+GO
