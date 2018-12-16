bf7f8cd1acebffa6a3d1aca070258c05015ead5e
Raphael <43172440+Raphael@users.noreply.github.com>
2018-12-16 21:07:24 +0100

    Added more tables

--- DungeonTyper/DungeonTyper.Web/Data/DataBase/Tables/BuildDungeonTyperTables.sql
+++ DungeonTyper/DungeonTyper.Web/Data/DataBase/Tables/BuildDungeonTyperTables.sql
@@ -7,6 +7,10 @@ IF EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'CharacterC
 GO
 IF EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Ability' AND TABLE_SCHEMA = 'DungeonTyper') DROP TABLE [DungeonTyper].[Ability]
 GO
+IF EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'CharacterClass_Abilities' AND TABLE_SCHEMA = 'DungeonTyper') DROP TABLE [DungeonTyper].[CharacterClass_Abilities]
+GO
+IF EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Character_Abilities' AND TABLE_SCHEMA = 'DungeonTyper') DROP TABLE [DungeonTyper].[Character_Abilities]
+GO
 
 CREATE TABLE [DungeonTyper].[CharacterClass] (
     [Id]              INT           IDENTITY (1, 1) NOT NULL,
@@ -25,6 +29,26 @@ CREATE TABLE [DungeonTyper].[Character] (
 CREATE TABLE [DungeonTyper].[Ability] (
 	[Id]                INT    IDENTITY (1, 1) NOT NULL,
     [AbilityName]	    VARCHAR (20)    NOT NULL,
+	[AbilityDescription] VARCHAR (100),
 	CONSTRAINT [PK_Ability] PRIMARY KEY CLUSTERED ([Id] ASC),
 );
+
+CREATE TABLE [DungeonTyper].[CharacterClass_Abilities] (
+	[Id]                INT    IDENTITY (1, 1) NOT NULL,
+    [CharacterClassId]	    INT    NOT NULL,
+	[AbilityId]				INT	   NOT NULL,
+	CONSTRAINT [PK_CharacterClass_Abilities] PRIMARY KEY CLUSTERED ([Id] ASC),
+	CONSTRAINT [FK_CharacterClass_Abilities_CharacterClass] FOREIGN KEY ([CharacterClassId]) REFERENCES [DungeonTyper].[CharacterClass] ([Id]),
+	CONSTRAINT [PK_CharacterClass_Abilities_Ability] FOREIGN KEY ([AbilityId]) REFERENCES [DungeonTyper].[Ability] ([Id])
+);
+
+CREATE TABLE [DungeonTyper].[Character_Abilities] (
+	[Id]                INT    IDENTITY (1, 1) NOT NULL,
+    [CharacterId]	    INT    NOT NULL,
+	[AbilityId]				INT	   NOT NULL,
+	CONSTRAINT [PK_Character_Abilities] PRIMARY KEY CLUSTERED ([Id] ASC),
+	CONSTRAINT [FK_Character_Abilities_Character] FOREIGN KEY ([CharacterId]) REFERENCES [DungeonTyper].[Character] ([Id]),
+	CONSTRAINT [FK_Character_Abilities_Ability] FOREIGN KEY ([AbilityId]) REFERENCES [DungeonTyper].[Ability] ([Id])
+);
+
 GO
--- DungeonTyper/DungeonTyper.Web/Data/DataBase/Tables/FillDungeonTyperTables.sql
+++ DungeonTyper/DungeonTyper.Web/Data/DataBase/Tables/FillDungeonTyperTables.sql
@@ -19,7 +19,19 @@ INSERT INTO [DungeonTyper].[Ability]([AbilityName]) VALUES('Charge')
 INSERT INTO [DungeonTyper].[Ability]([AbilityName]) VALUES('Flash Heal')
 INSERT INTO [DungeonTyper].[Ability]([AbilityName]) VALUES('Backstab')
 
+--FOREIGN KEYS
 --	CHARACTER (foreign key CharacterClassId)
 INSERT INTO [DungeonTyper].[Character]([CharacterClassId], [Alive]) VALUES (1, 1)
+
+-- CHARACTERCLASS_ABILITIES (characterlcassId, AbilityId)
+	-- Warrior
+INSERT INTO [DungeonTyper].[CharacterClass_Abilities]([CharacterClassId], [AbilityId]) VALUES (1, 2)
+INSERT INTO [DungeonTyper].[CharacterClass_Abilities]([CharacterClassId], [AbilityId]) VALUES (1, 4)
+	-- Mage
+INSERT INTO [DungeonTyper].[CharacterClass_Abilities]([CharacterClassId], [AbilityId]) VALUES (2, 1)
+	-- Rogue
+INSERT INTO [DungeonTyper].[CharacterClass_Abilities]([CharacterClassId], [AbilityId]) VALUES (3, 4)
+	-- Priest
+INSERT INTO [DungeonTyper].[CharacterClass_Abilities]([CharacterClassId], [AbilityId]) VALUES (4, 3)
 
 GO
