a81243fae2022f22e4708d698250ad9b2c23b6e4
Raphael <43172440+Raphael@users.noreply.github.com>
2019-01-07 23:28:32 +0100

    GROUP BY AND AGGREGATE DONE AND OUTER JOIN AND INNER JOIN DONE

---
+++ DungeonTyper/DungeonTyper.Web/Data/DataBase/StoredProcedures/GetAllCharacterClassAbilitiesCount.sql
@@ -0,0 +1,32 @@
+﻿-- =============================================
+-- Author:        <Raphael>
+-- Create date: <17/12/2018>
+-- Description:    <DungeonTyper, Killerapp Semester 2, >
+-- =============================================
+
+USE [dbi397017]
+GO
+
+IF EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_NAME = 'spCharacterClass_Abilities_GetAllCount' AND ROUTINE_SCHEMA = 'DungeonTyper') DROP PROCEDURE [DungeonTyper].[spCharacterClass_Abilities_GetAllCount]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [DungeonTyper].[spCharacterClass_Abilities_GetAllCount]
+AS
+BEGIN
+    SET NOCOUNT ON;
+SELECT 
+    [cc].[ClassName], COUNT([ccabs].[AbilityId]) AS [AbilityCount]
+FROM 
+	[DungeonTyper].[CharacterClass_Abilities] AS [ccabs],
+	[DungeonTyper].[CharacterClass] AS [cc]
+WHERE 
+	[cc].[Id] = [ccabs].[CharacterClassId]
+GROUP BY 
+	[cc].[ClassName]
+END
+GO
--- DungeonTyper/DungeonTyper.Web/Data/DataBase/Tables/FillDungeonTyperTables.sql
+++ DungeonTyper/DungeonTyper.Web/Data/DataBase/Tables/FillDungeonTyperTables.sql
@@ -2,10 +2,11 @@
 GO
 
 --DELETE DATA
-DELETE FROM [DungeonTyper].[Character]
-DELETE FROM [DungeonTyper].[CharacterClass]
+
 DELETE FROM [DungeonTyper].[CharacterClass_Abilities]
 DELETE FROM [DungeonTyper].[Ability]
+DELETE FROM [DungeonTyper].[CharacterClass]
+DELETE FROM [DungeonTyper].[Character]
 
 --INSERT
 --	CHARACTERCLASS
@@ -21,6 +22,9 @@ INSERT INTO [DungeonTyper].[Ability]([AbilityName]) VALUES('Flash Heal')
 INSERT INTO [DungeonTyper].[Ability]([AbilityName]) VALUES('Backstab')
 INSERT INTO [DungeonTyper].[Ability]([AbilityName]) VALUES('Stomp')
 INSERT INTO [DungeonTyper].[Ability]([AbilityName]) VALUES('Dodge')
+INSERT INTO [DungeonTyper].[Ability]([AbilityName]) VALUES('Pierce')
+INSERT INTO [DungeonTyper].[Ability]([AbilityName]) VALUES('Slash')
+
 
 --FOREIGN KEYS
 --	CHARACTER (foreign key CharacterClassId)
@@ -29,7 +33,7 @@ INSERT INTO [DungeonTyper].[Character]([CharacterName], [CharacterClassId], [Ali
 -- CHARACTERCLASS_ABILITIES (characterlcassId, AbilityId)
 	-- Warrior
 INSERT INTO [DungeonTyper].[CharacterClass_Abilities]([CharacterClassId], [AbilityId]) VALUES (1, 2)
-INSERT INTO [DungeonTyper].[CharacterClass_Abilities]([CharacterClassId], [AbilityId]) VALUES (1, 4)
+INSERT INTO [DungeonTyper].[CharacterClass_Abilities]([CharacterClassId], [AbilityId]) VALUES (1, 5)
 	-- Mage
 INSERT INTO [DungeonTyper].[CharacterClass_Abilities]([CharacterClassId], [AbilityId]) VALUES (2, 1)
 	-- Rogue
